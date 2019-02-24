2237b5d7cb3528a56a22f5dbedde22b59f7ce10b
choong liang <choongli@buffalo.edu>
2019-02-24 16:26:12 -0500

    couldnt get session working
    login credentials verified.
    Current error: passport session error

--- models/users.js
+++ models/users.js
@@ -25,8 +25,33 @@ var userSchema = mongoose.Schema({
 var User = module.exports = mongoose.model('User',userSchema);
 
 module.exports.createUser = function(newUser, callback){
-    var salt = bcrypt.genSaltSync(10);
-    //var hash = bcrypt.hashSync(newUser.password,salt);
-    newUser.password=bcrypt.hashSync(newUser.password,salt);
-    newUser.save(callback);
+    // Async
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(newUser.password, salt, function(err, hash) {
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+
+    // Sync
+    // var salt = bcrypt.genSaltSync(10);
+    // //var hash = bcrypt.hashSync(newUser.password,salt);
+    // newUser.password=bcrypt.hashSync(newUser.password,salt);
+    // newUser.save(callback);
+};
+
+module.exports.getUserByUsername = function(username, callback){
+    var query = {username: username}; // username should match
+    User.findOne(query,callback);
+};
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+    bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+        if (err) throw (err);
+        callback(null, isMatch);
+    });
+};
+
+module.exports.getUserByID = function(id, callback){
+    User.findById(id,callback); //mongodb function
 };
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
-var user = require('../models/users');
+var User = require('../models/users');
 
 /* GET register page. */
 router.get('/register', function(req, res, next) {
@@ -21,21 +23,61 @@ router.post('/register', function(req, res, next) {
   var email = req.body.emailaddress;
   var password = req.body.password;
 
-  var newUser = new user({
+  var newUser = new User({
     username: id,
     name: name,
     email: email,
     password: password
   });
 
-  user.createUser(newUser,function(err,user){
+  User.createUser(newUser,function(err,user){
     if(err) throw err;
     console.log(user);
   });
 
-  req.flash('success_msg','Register successfully.');
-
   res.redirect('/users/login');
 });
+
+/* login via passport js */
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+      User.getUserByUsername(username, function(err, user){
+        if(err) throw(err);
+        if(!user){
+          return done(null, false);
+        }
+
+        User.comparePassword(password, user.password, function(err, isMatch){
+          if(err) throw (err);
+          if(isMatch){
+            return done(null,user);
+          }else{
+            return done(null, false);
+          }
+        });
+
+      });
+    }
+));
+
+/* Serialize and Deserialize User */
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserByID(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+router.post('/login',
+    passport.authenticate('local', {successRedirect:'/index', failureRedirect:
+    '/users/login', failureFlash: true}),
+    function(req, res) {
+      // If this function gets called, authentication was successful.
+      // `req.user` contains the authenticated user.
+      res.redirect('/');
+    });
 
 module.exports = router;
